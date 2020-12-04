712eb32e51ba2748e34681dbb85740ee23af0f0c
sam_el-kamand <sam.elkamand@gmail.com>
2020-12-04 14:37:02 +1100

    moved package to root directory

--- utilitybelt/utilitybelt.Rcheck/00_pkg_src/utilitybelt/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(utilitybelt)
-
-test_check("utilitybelt")
--- utilitybelt/utilitybelt.Rcheck/00_pkg_src/utilitybelt/tests/testthat/test-here.R
+++
@@ -1,4 +0,0 @@
-test_that("positiveControl_checkScriptsDeclareTheirLocation", {
-  print(getwd())
-  testthat::expect_true(checkScriptsDeclareTheirLocation("here_positive_control"))
-})

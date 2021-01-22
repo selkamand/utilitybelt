5f904ff3c791a4e410dedd061507d4ee6c80ce0a
sam_el-kamand <sam.elkamand@gmail.com>
2021-01-22 14:23:29 +1100

    fixed theme_axis_titles function

--- R/ggextensions.R
+++ R/ggextensions.R
@@ -8,7 +8,6 @@
 #' @export
 #'
 theme_axis_titles <- function(){
-  ggpubr::theme_cleveland() + 
   ggplot2::theme(plot.title = ggplot2::element_text(hjust=0.5, size = 18, face = "bold"),
         axis.title = ggplot2::element_text(face="bold"), 
         axis.title.y = ggplot2::element_text())

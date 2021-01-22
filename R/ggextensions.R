cb80866d860119254298ec9745ab4666408a4c08
sam_el-kamand <sam.elkamand@gmail.com>
2021-01-22 16:11:33 +1100

    added more ggextensions for legend manipulation

--- R/ggextensions.R
+++ R/ggextensions.R
@@ -25,3 +25,21 @@ theme_axis_titles_cleveland <- function(){
           axis.title = ggplot2::element_text(face="bold"), 
           axis.title.y = ggplot2::element_text())
 }
+
+#' Custom Themes
+#'
+#' @return ggtheme
+#' @export
+#'
+theme_no_legend <- function(){
+    ggplot2::theme(legend.position = "none")
+}
+
+#' Custom Themes
+#'
+#' @return ggtheme
+#' @export
+#'
+theme_no_legend_title <- function(){
+  ggplot2::theme(legend.title = element_blank())
+}
